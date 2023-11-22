de1df2113c943ec79496ea050e28ea403d4e8944
Maftuna Vohidjonovna Domirando <maisiedev.gmail.com>
2023-11-22 12:31:28 +0500

    permission is given to read a pdf

--- app/src/main/java/com/example/handybook/ui/EBookFragment.kt
+++ app/src/main/java/com/example/handybook/ui/EBookFragment.kt
@@ -1,11 +1,17 @@
 package com.example.handybook.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import coil.load
 import com.example.handybook.R
 import com.example.handybook.adapters.ViewPagerAdapter
@@ -74,14 +80,35 @@ class EBookFragment : Fragment() {
 //                .replace(R.id.main, PDFViewerFragment.newInstance(id.toString(),""))
 //                .commit()
 //        }
+
         var bundle = Bundle()
         bundle.putInt("id",id)
+
+
+
+
         binding.read.setOnClickListener{
-            var pdf = PdfViewFragment()
-            pdf.arguments = bundle
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.main, pdf)
-                .commit()
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
+                val pdfFileUri: Uri = Uri.parse(book.file)
+                Log.d("pdf uri", pdfFileUri.path.toString())
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(pdfFileUri, "application/pdf")
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(intent)
+            } else {
+                Log.d("permi", "denied")
+            }
+
+
+
+
+
+            val packageManager: PackageManager? = requireActivity().packageManager
+//            if (intent.resolveActivity(packageManager!!) != null) {
+//                startActivity(intent)
+//            } else {
+//                Log.d("pdf uri packagemanager null", pdfFileUri.path.toString())
+//            }
         }
         binding.linearLayout5.setOnClickListener {
             var details = ReviewFragment()
